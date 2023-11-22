d9629d1754a00d48f1bab96e02163e1caf167259
shivangiag08 <shivangiag08@gmail.com>
2023-11-22 18:49:19 +0530

    adding flipped images

--- flipper.py
+++ flipper.py
@@ -4,18 +4,10 @@ import numpy as np
 # read all images for folder name abc and flip them horizontally and save them in the same folder
 
 folder_name = 'Collected Dataset'
-for i in range(18, 99):
+for i in range(135,208):
     try:
-        img = cv2.imread(f'{folder_name}/FLIR00{i}.jpg')
+        img = cv2.imread(f'{folder_name}/{i}.jpg')
     except:
         FileNotFoundError
     img = cv2.flip(img, 1)
-    cv2.imwrite(f'Collected Dataset/flipped/FLIR{i}.jpg', img)
-
-for i in range(100, 253):
-    try:
-        img = cv2.imread(f'{folder_name}/FLIR00{i}.jpg')
-    except:
-        FileNotFoundError
-    img = cv2.flip(img, 1)
-    cv2.imwrite(f'Collected Dataset/flipped/FLIR{i}.jpg', img)
+    cv2.imwrite(f'Collected Dataset/flipped/{i}_flipped.jpg', img)
