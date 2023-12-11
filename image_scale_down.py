ac05d56adf5e03a305084ff8c0a3162b1b9d2394
shivangiag08 <shivangiag08@gmail.com>
2023-12-11 22:12:43 +0530

    ""

--- image_scale_down.py
+++ image_scale_down.py
@@ -13,12 +13,12 @@ def scaleImage(img, width, height):
     return scaled_img
 
 def main():
-    for i in range(0, 414):
-        img = cv2.imread(f'/Users/tanmay/Documents/GitHub/ILGC-3-Project/Collected Dataset/{i}.jpg')
+    for i in range(415, 441):
+        img = cv2.imread(f'Collected Dataset\scaled_down\{i}.jpg')
         if img is None:
             continue
         scaled_img = scaleImage(img, 32, 24)
-        cv2.imwrite(f'/Users/tanmay/Documents/GitHub/ILGC-3-Project/Collected Dataset/scaled_down/{i}.jpg', scaled_img)
+        cv2.imwrite(f'Collected Dataset/scaled_down/{i}.jpg', scaled_img)
         
 
 if __name__ == "__main__":
